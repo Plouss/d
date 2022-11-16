7e9e8f0c2b1f617761cc6cc3f1d0be671370b3a2
Plouss <80962741+Plouss@users.noreply.github.com>
2022-11-16 17:02:00 +0100

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oussama <oussama@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 15:03:37 by ouel-maj          #+#    #+#             */
+/*   Updated: 2022/11/16 16:13:40 by oussama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <limits.h>
+
+int		ft_printchar(char c);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+int		ft_checklen(int n);
+int		ft_printnbr(int len);
+char	*ft_itoa(int n);
+int		ft_printstr(char *str);
+int		ft_printf(const char *str, ...);
+#endif
